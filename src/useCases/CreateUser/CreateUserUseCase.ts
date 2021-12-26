3d23e759c2f9dbdf3d94c201e62030c1a8376ac2
pedro-demeu <pdemeu@segware.com>
2021-12-26 11:58:53 -0300

    chore: add interface IMailProvider

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,8 +1,13 @@
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { ICreateUserRequestDTO } from "./CreateUserDTO";
 import { User } from "../../entity/User";
+import { IMailProvider } from "../../providers/IMailProvider";
 export class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(
+    private usersRepository: IUsersRepository,
+    private mailProvider: IMailProvider
+  ) {}
+
   async execute(data: ICreateUserRequestDTO) {
     const userAlreadyExists = await this.usersRepository.findByEmail(
       data.email
@@ -13,5 +18,18 @@ export class CreateUserUseCase {
 
     const user = new User(data);
     await this.usersRepository.save(user);
+
+    this.mailProvider.sendMail({
+      to: {
+        name: data.name,
+        email: data.email,
+      },
+      from: {
+        name: "Equipe do meu App",
+        email: "equipe@meuapp.com",
+      },
+      subject: "Seja Bem-vindo à Plataforma",
+      body: "<p>Você já pode fazer login em nossa plataforma.</p>",
+    });
   }
 }
